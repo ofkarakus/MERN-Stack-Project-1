59c902a8a5e2d629c66a4610a3e6e38d43ddc00e
ofkarakus <omerfarukkarakus01@gmail.com>
2020-12-13 14:00:36 +0000

    feat: add user model

--- server.js
+++ server.js
@@ -3,8 +3,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const {connect_db} = require("./models/db")
-const port = process.env.PORT || 5001;
+const {connect_db} = require("./models/DB")
 
 connect_db()
 
@@ -12,6 +11,6 @@ connect_db()
 const router = require("./routes/router");
 app.use("/api", router); //  respond only "/api" endpoint
 
-app.listen(port, () => {
-  console.log(`I'm listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`I'm listening on port ${process.env.PORT}`);
 });
