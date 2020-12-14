b339a2cd5189b19f18d6cf2a0462a82d9d932bfb
ofkarakus <omerfarukkarakus01@gmail.com>
2020-12-14 09:29:58 +0000

    feat: crypt password (bcryptjs package)

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,25 +1,26 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 exports.sign_up = async (req, res) => {
   // TODO: Register func.
   const { firstName, lastName, email, password } = req.body;
-  console.log(firstName, lastName, email, password);
 
-  // TODO: save the user to DB
+  // TODO: validate the fields
+  // TODO: check already registered
+
+  // TODO: crypt password
+  const salt = await bcrypt.genSalt(10);
+  const cryptedPassword = await bcrypt.hash(password, salt);
 
   const user = new User({
     firstName, // => firstName: firstName  (ES6)
     lastName,
     email,
-    password, // :crypted password
+    password: cryptedPassword,
   });
 
   await user.save();
   // TODO: error handling for saving
-
-  // TODO: validate the fields
-  // TODO: check already registered
-  // TODO: crypt password
 
   res.send("SignUp Successful");
 };
