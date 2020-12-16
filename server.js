24e705987e725a134ac9b271ebaa712a46ee3105
ofkarakus <omerfarukkarakus01@gmail.com>
2020-12-16 12:04:36 +0000

    add settings for production

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
+const path = require("path")
 const { connect_db } = require("./models/DB");
 connect_db();
 app.use(express.json());
@@ -12,6 +13,14 @@ app.use(express.json());
 const router = require("./routes/router");
 app.use("/api", router); //  respond only "/api" endpoint
 
+// FOR PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
-  console.log(`I'm listening on port ${process.env.PORT}`);
+  console.log(`I'm listening on port ${port}`);
 });
